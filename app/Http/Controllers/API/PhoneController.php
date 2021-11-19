a2a98fd233818f9cb9f3aacc8e0c80e225b6d4a3
pumpunya <pumpunya1942@gmail.com>
2021-11-19 10:43:44 +0700

    API

---
+++ app/Http/Controllers/API/PhoneController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\phone;
+
+class SizeimageController extends Controller
+{
+    public function index()
+    {
+        $phone = phone::all(); 
+        return response()->json($phone);
+    }
+
+}
