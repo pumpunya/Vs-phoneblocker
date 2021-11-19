eed13fd5605d3bf70ee46654b3aab1d8e78a41ea
pumpunya <pumpunya1942@gmail.com>
2021-11-19 10:59:01 +0700

    a

--- app/Http/Controllers/API/PhoneController.php
+++ app/Http/Controllers/API/PhoneController.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\phone;
 
-class SizeimageController extends Controller
+class phoneController extends Controller
 {
     public function index()
     {
