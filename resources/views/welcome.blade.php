8305678563d54d4429be4ed1fa603614c1de9227
pumpunya <pumpunya1942@gmail.com>
2021-12-02 15:25:54 +0700

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href= "image/logo.png" />
     <title>PHONEBLOCKER</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
@@ -79,7 +80,7 @@
     <br>
     <br><br>
     <br><br>
-    <img src="https://cdn-icons.flaticon.com/png/512/2574/premium/2574172.png?token=exp=1638431117~hmac=e7d9dcb14e68fb012232fc435127cee1" width= 80 height=100 class="img-fluid">
+    <img src="image/logo.png" width="250" height="250"  alt="center" >
     <section id="service" class="py-5">
         <div class="container">
             <div class="row">
