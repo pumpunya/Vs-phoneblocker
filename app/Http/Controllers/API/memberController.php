9c54136641750c651d8b153eb660332001f09591
yoneya <Yoneya.ohm1221@gmail.com>
2022-01-25 16:29:50 +0700

    use db member

--- app/Http/Controllers/API/memberController.php
+++ app/Http/Controllers/API/memberController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\member;
+use DB;
 
 class memberController extends Controller
 {
